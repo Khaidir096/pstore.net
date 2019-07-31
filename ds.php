75375e53bc5ba7722c28df7f0074f3fb0eaa15da
Khaidir096 <40510753+Khaidir096@users.noreply.github.com>
2019-07-31 09:31:30 +0000

    Add files via upload

---
+++ ds.php
@@ -0,0 +1,14 @@
+<?php
+// Warna Terminal
+$biru = "\e[34m";
+$kuning = "\e[33m";
+$cyan = "\e[96m";
+$magenta = "\e[35m";
+$hijau = "\e[92m";
+$m = "\e[91m";
+$b = " \033[1m";
+
+echo " $cyan + $b AUTO VISITOR VERSI ll (limited edition):D \n ";
+echo " $m INFO! ";
+echo file_get_contents("https://pastebin.com/raw/S5PZ0EbK");
+?>
